74cc74b2201039b0de6c8589c221f93a2766be6a
Gibran Ojeda <gibran.muppers@gmail.com>
2022-09-09 13:12:07 -0500

    Ejemplo SVG-MAPS/México

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import * as Mexico from '@svg-maps/mexico'
+// const Mexico = require('@svg-maps/mexico');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'svg-map-mexico';
+  public Mexico: any = Mexico;
+  public state: String = "";
+
+  clickLocation(event: Event){
+    let location = this.getLocationName(event.target)
+    this.state = location
+  }
+
+  changeText(event: Event){
+    let location = this.getLocationName(event.target)
+    this.state = location
+  }
+
+  getLocationName(node:any) {
+    return node && node.attributes.name.value
+  }
+
 }
