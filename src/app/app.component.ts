8cda119a0f0538104fd840dcbd1aac90e5eba4c7
Gibran Ojeda <gibran.muppers@gmail.com>
2022-10-07 12:40:52 -0500

    Corrección de funcionamiento en Label Ejecutivo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,12 @@ import * as Mexico from '@svg-maps/mexico'
 export class AppComponent {
   public Mexico: any = Mexico;
   public state: String = "";
-  public tooltipStyle:any;
+  public tooltipStyle:any = {
+    display: 'none',
+    top: `0px`,
+    left: `0px`,
+  }
+  public currentLocation:String = ""
 
   clickLocation(event: Event){
     let location = this.getLocationName(event.target)
@@ -29,10 +34,25 @@ export class AppComponent {
   }
 
   moveOnLocation(event:any) {
-    this.tooltipStyle = {
-      display: 'block',
-      top: `${event.clientY + 5}px`,
-      left: `${event.clientX - 5}px`,
+    let location = this.getLocationName(event.target)
+    // console.log(`X: ${event.clientX} - Y: ${event.clientY} IN LOC: ${location}`)
+    if(this.currentLocation !== location){
+      this.currentLocation = location
+      this.tooltipStyle = {
+        display: 'block',
+        top: `${event.clientY-5}px`,
+        left: `${event.clientX-5}px`,
+      }
+    }
+  }
+
+  watchPointer(event:any){
+    if(event.path[0].attributes.id===undefined){
+      this.tooltipStyle = {
+        display: 'none',
+        top: `${event.clientY}px`,
+        left: `${event.clientX}px`,
+      }
     }
   }
 
